72b1f499075b5440717e38ffa9a4135e0565a050
Andrew Zhang <andrewzhang1101@gmail.com>
2023-01-11 16:33:36 -0500

    added fleeing and throwing for carriers sphere1_11_1

---
+++ src/sphere1_11_1/BoosterRobot.java
@@ -0,0 +1,15 @@
+package sphere1_11_1;
+
+import battlecode.common.*;
+import sphere2.Robot;
+
+public strictfp class BoosterRobot extends Robot {
+    
+    public BoosterRobot(RobotController rc) {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {
+
+    }
+}
---
+++ src/sphere1_11_1/DestabilizerRobot.java
@@ -0,0 +1,15 @@
+package sphere1_11_1;
+
+import battlecode.common.*;
+import sphere2.Robot;
+
+public strictfp class DestabilizerRobot extends Robot {
+    
+    public DestabilizerRobot(RobotController rc) {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {
+
+    }
+}
