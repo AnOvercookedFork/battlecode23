3a33cd3175ec4ea98228ee0f972c54718bb107d2
Brian Wang <oldamazingbrain@gmail.com>
2023-02-03 23:46:54 -0500

    fixed pain.py

--- optimizer/pain.py
+++ optimizer/pain.py
@@ -7,7 +7,7 @@ import signal
 
 # these constants are probably fine
 maps = ['AbsoluteW','AllElements','ArtistRendition','Barcode','BatSignal','BattleSuns','BowAndArrow','Buggy','Cat','Cave','Cee','Checkmate2','Clown','Contraction','Cornucopia','Crossword','Cube','DefaultMap','Diagonal','Divergence','Dreamy','Eyelands','Flower','Forest','FourNations','Frog','Grapes','Grievance','Hah','Heart','HideAndSeek','HorizontallySymmetric','HotAirBalloon','IslandHopping','IslandHoppingTwo','Jail','KingdomRush','Lantern','LightWork','Lines','maptestsmall','Marsh','MassiveL','Maze','Minefield','Movepls','Orbit','PairedProgramming','Pakbot','Pathfind','Piglets','Pit','Pizza','Potions','Quiet','RaceToTheTop','Rainbow','Rectangle','Repetition','Resign','Rewind','Risk','River','RockWall','Sakura','Scatter','Sine','SmallElements','Sneaky','Snowflake','SomethingFishy','SoundWave','Spin','Spiral','Squares','Star','Sun','Sus','SweetDreams','Tacocat','Target','ThirtyFive','TicTacToe','Tightrope','TimesUp','TreasureMap','Turtle','USA','VerticallySymmetric']
-max_subprocesses = 1
+max_subprocesses = 12
 
 # set these
 teamA = 'klein_bottle2_02_2'
@@ -22,6 +22,7 @@ curTA = ''
 curTB = ''
 
 def get_winner(data):
+    print(data)
     # does weird stuff if client is open, opened, or closed
     result = data[-5].split(' ')[-4][1:2]
     if result == 'A' or result == 'B':
@@ -32,21 +33,24 @@ def get_winner(data):
     return data[-7].split(' ')[-4][1:2]
 
 def run_matches(teamA, teamB, score):
-    curtA = teamA
-    curtB = teamB
+    curTA = teamA
+    curTB = teamB
 
     map_score = {}
     for map in maps:
         
         print('Running map {}'.format(map))
-        processes.add(subprocess.check_output('gradlew run -Pmaps={map} -PteamA={a} -PteamB={b} -Pdebug=false -Penableprofiler=false -PoutputVerbose=false -PshowIndicators=false'.format(map = map, a = teamA, b = teamB), shell=True))
+        processes.add(subprocess.Popen('gradlew run -Pmaps={map} -PteamA={a} -PteamB={b} -Pdebug=false -Penableprofiler=false -PoutputVerbose=false -PshowIndicators=false -Dbc.server.websocket=false'.format(map = map, a = teamA, b = teamB), stdout=subprocess.PIPE, text=True, shell=True))
         print(len(processes))
         while(len(processes) >= max_subprocesses):
             time.sleep(1)
+            to_remove = []
             for p in processes:
                 if p.poll() is not None:
-                    out = p.output.read().splitlines()
-                    winner = get_winner(out)
+                    out, err = p.communicate()
+                    #print(out)
+                    #print(err)
+                    winner = get_winner(out.split('\n'))
                     if winner == 'A':
                         score[0] += 1
                         map_score[map] = teamA
@@ -57,7 +61,9 @@ def run_matches(teamA, teamB, score):
                         print('I don\'t know who won this, take a look: {}'.format(out))
                     current_score = score
                     current_map_score = map_score
-                    processes.remove(p)
+                    to_remove.append(p)
+            for p in to_remove:
+                processes.remove(p)
         
     while(len(processes) > 0):
         time.sleep(1)
@@ -80,15 +86,21 @@ def run_matches(teamA, teamB, score):
 
     return score, map_score
 
-def sigint_handler():
-    print('Current score: {} {} {}'.format(current_score, curTA, curTB))
+def sigint_handler(sig, frame):
+    print('Sigint received, exiting...')
+    for p in processes:
+        p.kill()
+    print('Current score: {} {} {}'.format(current_score, curTA, curTB)) # borked, does not actually update
     print(current_map_score)
     exit()
 
 def main():
     os.chdir('..')
+    os.system('setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_351"')
     os.system('gradlew update')
     os.system('gradlew build')
+
+    signal.signal(signal.SIGINT, sigint_handler)
 
     current = time.time()
     score1 = run_matches(teamA, teamB, [0, 0])
